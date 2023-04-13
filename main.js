8702e8c0869a1d3518cda0762a625b2b65798dbd
shivanshshourya4 <92980002+shivanshshourya4@users.noreply.github.com>
2023-04-13 23:26:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function alertUser()
+{
+    alert("This website is legit or real.\nMany people have lost their life because of the tricks.");
+    
+}
+
+function pp()
+{
+ alert("Privacy Poilicy-\nPlease don't die")
+}
+
+function tou() 
+{
+    alert("Terms Of Use-\nBe Happy and Enjoy life!")
+}
+
+function b_rom()
+{
+ document.getElementById("main_part").style="filter:blur(0px);";
+ document.getElementById("btn_main").style="visibility: hidden;"
+}
